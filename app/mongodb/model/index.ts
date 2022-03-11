f7bd318bbab1261138ef319c6f9b60f742304043
esl1129 <esl112999@gmail.com>
2022-03-11 15:09:20 +0900

    Update index.ts

--- app/mongodb/model/index.ts
+++ app/mongodb/model/index.ts
@@ -4,9 +4,10 @@ const mongoose = require('mongoose');
 // Use Node.js native promise 
 mongoose.Promise = global.Promise;
 
-const db = {};
-db.mongoose = mongoose;
-db.url = dbConfig.url;
-db.tutorial = require('./model.ts')(mongoose);
+const db = {
+    mongoose : mongoose,
+    url : dbConfig.url,
+    tutorial : require('./model.ts')(mongoose)
+};
 
 module.exports = db;

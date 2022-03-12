d63b1f8cc4edc79e47ef5a5026538bc581fa9fc7
Jaewook Lim <esl112999@gmail.com>
2022-03-12 12:59:22 +0900

    Add Comment GCP Cloud Comment

--- app/mongodb/config/config.ts
+++ app/mongodb/config/config.ts
@@ -1,3 +1,5 @@
-module.exports = { 
-    url: `mongodb+srv://esl1129:wodnr5908@woogiecluster.g94at.mongodb.net/test?retryWrites=true&w=majority`
+// GCP Cloud Compute
+
+module.exports = {
+  url: `mongodb+srv://esl1129:wodnr5908@woogiecluster.g94at.mongodb.net/test?retryWrites=true&w=majority`,
 };
